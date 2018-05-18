d148728ad31e89cd9eac079bab24f2179d01d70b
dannyb21892 <dannyb21892@verizon.net>
2018-05-18 19:28:24 -0400

    working search on navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,49 @@
 import React from "react"
 
 class NavBar extends React.Component {
+  state = {
+    search: ""
+  }
+
+  handleChange = e => {
+    if(e.key === "Enter"){
+      this.search()
+    } else if (e.key === "Backspace"){
+      this.setState({
+        search: this.state.search.slice(0,-1)
+      })
+    } else {
+      this.setState({
+        search: this.state.search+e.key
+      })
+    }
+  }
+
+  search = () => {
+    fetch("http://localhost:3000/api/v1/games",{
+      method: "POST",
+      headers: {
+       'Content-type':'application/json'
+      },
+      body: JSON.stringify({
+        type: "checkGame",
+        title: this.state.search
+      })
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
+
   render() {
-    let logInOrOut = this.props.loggedIn ? <li class="button"><a onClick={this.props.logout} class="button special">Log Out</a></li> : <li class="button special"><a href="/login" class="button special">Sign Up or Log In</a></li>
+    let logInOrOut = this.props.loggedIn ? <li className="button"><a onClick={this.props.logout} className="button special">Log Out</a></li> : <li className="button special"><a href="/login" className="button special">Sign Up or Log In</a></li>
+    let profile = this.props.loggedIn ? <li className="button"><a onClick={() => window.location.href = `http://localhost:3001/users/${localStorage.getItem("username")}`} className="button special">{localStorage.getItem("username")}</a></li> : null
     return (
-      <header id="header" class="skel-layers-fixed">
+      <header id="header" className="skel-layers-fixed">
 				<h2><a href="/">WikiSpeedia</a></h2>
 				<nav id="nav">
 					<ul>
-						<li><a href="/">Home</a></li>
-						<li><a href="left-sidebar.html">Left Sidebar</a></li>
-						<li><a href="right-sidebar.html">Right Sidebar</a></li>
-						<li><a href="no-sidebar.html">No Sidebar</a></li>
+						<li><input type="text" placeholder="find a game" value={this.state.search} onKeyDown={this.handleChange}/></li>
+						{profile}
 						{logInOrOut}
 					</ul>
 				</nav>
